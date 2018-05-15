df053fa1e654dd7dcdc50e883996fc4d83e43eec
Rubén Izquierdo Belinchón <Rubenizq@ucm.es>
2018-05-15 14:06:09 +0200

    Subida de recursos

---
+++ styles/MUNICIPIOSCASTILLALAMANCHA_2_style.js
@@ -0,0 +1,28 @@
+var size = 0;
+var placement = 'point';
+
+var style_MUNICIPIOSCASTILLALAMANCHA_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "10.4px \'MS Shell Dlg 2\', sans-serif";
+    var labelFill = "rgba(0, 0, 0, 1)";
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("NAME_TAB") !== null) {
+        labelText = String(feature.get("NAME_TAB"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(83,196,70,0.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+
+    return style;
+};
